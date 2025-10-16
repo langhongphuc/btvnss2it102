02d3e4aaad80fdb85ea6b5aaca0f4fe4082458ad
langhongphuc <langphuc27@gmail.com>
2025-10-16 09:26:48 +0700

    LangHongPhuc_B25DTCN199_IT102_SS2_btvn1

---
+++ bt1ss2it102.h
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+
+int main() {
+    short a = 100; // so nguyen nho
+	int b = 100; // so nguyen thuong
+	long c = 1000; // so nguyen lon
+	long long d = 10000; // so nguyen rat lon
+	unsigned short e = 20; // so nguyen duong nho
+	unsigned int f = 200; // so nguyen duong
+	unsigned long g = 2000; // so nguyen duong lon
+	unsigned long long h = 20000; so nguyen duong rat lon
+	char ch = 'A'; // ky tu
+	
+	 printf("%d %d %ld %lld %hu %u %lu %llu %c", a, b, c, d, e, f, g, h, ch);
+	 return 0;
+}
