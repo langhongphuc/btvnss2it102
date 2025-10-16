29b6a4876e0e0ec42dd88b9947b36974982fb2b5
langhongphuc <langphuc27@gmail.com>
2025-10-16 19:31:26 +0700

    LangHongPhuc_B25DTCN199_IT102_SS2_BTVN5

---
+++ bt5ss2it102.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main (){
+	int numberA, numberB;
+	float numberC;
+	
+	numberA = 5;
+	numberB = 7;
+	numberC = 2.25;
+	
+	float sumOfABC = numberA + numberB + numberC;
+	
+	int sumIntOfABC = (int)(nummberA + numberB + numberC);
+	
+	 printf("Tong cua 3 so theo kieu float: %f", sumOfABC);
+	 printf("\nTong cua 3 so theo kieu int: %d", sumIntOfABC); 
+}
