33e1c33b2521689f05e3472966c0e9a8250e9dea
langhongphuc <langphuc27@gmail.com>
2025-10-16 19:11:07 +0700

    LangHongPhuc_B25DTCN199_IT102_SS2_BTVN4

---
+++ bt4ss2it102.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	int chieuDai = 10, chieuRong = 5;
+	int chuVi, dienTich;
+	
+	chuVi = 2 * (chieuDai + chieuRong);
+	dienTich = chieuDai * chieuRong;
+	
+	printf("Chu vi: %d\n" chuVi);
+	printf("Dien tich:%d\n", dienTich);
+	
+	return 0; 
+}
