d62e93023fbe7d23f68a53fb1756ecb0d0469721
langhongphuc <langphuc27@gmail.com>
2025-10-16 15:31:24 +0700

    LangHongPhuc_B25DTCN199_IT102_SS2_BTVN2

---
+++ bt2ss2it102.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+
+int main() {
+	int a = 5, b = 7; // khai bao va khoi tao hai bien so nguyen
+	int tong = a + b; // tinh tong và luu vao bien khac
+	
+	printf("Tong la: %d\n", tong); // in ket qua ra man hinh
+	return 0; 
+}
